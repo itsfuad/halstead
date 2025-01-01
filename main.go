59b7f1e41aa01e9343164bfc1d4c7cfbad71e9dc
Fuad Hasan <fuad.cs22@gmail.com>
2025-01-01 22:32:57 +0600

    Add CLI input handling and update README; add .gitignore and remove unused lexer

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"flag"
 )
 
 type LanguageRule struct {
@@ -366,9 +367,28 @@ func GetHalsteadMetrics(tokens []Token) Halstead {
 	return h
 }
 
+//cli input from args
+func getCLIInput() string {
+	var filepath string
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "  %s -filepath <filepath>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&filepath, "filepath", "", "Filepath to the source code")
+	flag.Parse()
+
+	if filepath == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	return filepath
+}
+
 func main() {
 
-	filepath := "Dataset/PalindromeChecker.java"
+	filepath := getCLIInput()
 
 	metrics, err := analyzeSourceCode(filepath)
 	if err != nil {
